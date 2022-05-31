c59b8566dce022ba3069fef90cb73150690879b4
Fardin Ahsan <fardin.ahsan75@gmail.com>
2022-06-01 01:41:46 +0600

    init commit

---
+++ public/js/offline.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  var body = document.querySelector('body');
+
+  //After DOM Loaded
+  document.addEventListener('DOMContentLoaded', function (event) {
+    //On initial load to check connectivity
+    if (!navigator.onLine) {
+      updateNetworkStatus();
+    }
+
+    window.addEventListener('online', updateNetworkStatus, false);
+    window.addEventListener('offline', updateNetworkStatus, false);
+  });
+
+  //To update network status
+  function updateNetworkStatus() {
+    if (navigator.onLine) {
+			body.classList.remove('offline');
+			console.log('Online');
+    } else {
+			body.classList.add('offline');
+			console.log('Offline');
+    }
+  }
+})();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Progrssive Web App - Starter
+
+A simple PWA demo utilizing an app manifest, service workers and caching.
+
+This demo is written in plain-vanilla HTML, CSS and Javascript (ES6).
+
+It has no dependencies.
+
+Change background color and text color when offline
+
+## Demo
+https://progwebapp.web.app/
+
+## Audits
+<img src="lighthouse-audit.png">
